90d955360fb1617837627eeeee50becaec46f873
patryk <patrykb511@gmail.com>
2017-01-14 13:25:01 +0100

    Added "open file" and "save" windows

--- src/sample/View.java
+++ src/sample/View.java
@@ -1,28 +1,64 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class View extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        //arent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
         VBox root = new VBox();
-        primaryStage.setTitle("Hello World");
 
         MenuButton dropdownFile = new MenuButton("Datei");
+        MenuItem menuOpen = new MenuItem("Öffnen");
+        MenuItem menuSave = new MenuItem("Speichern");
         dropdownFile.getItems().addAll(
-                new MenuItem("Öffnen"),
-                new MenuItem("Speichern")
+                menuOpen,
+                menuSave
         );
         ToolBar toolBar = new ToolBar(
             dropdownFile
         );
         root.getChildren().add(toolBar);
 
+        Label labelSelectedDirectory = new Label("Dir");
+        root.getChildren().add(labelSelectedDirectory);
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Datei öffnen:");
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Speichern unter:");
+
+        menuOpen.setOnAction(event -> {
+            fileChooser.showOpenDialog(primaryStage);
+            File selectedDirectory = directoryChooser.showDialog(primaryStage);
+            labelSelectedDirectory.setText(selectedDirectory.getAbsolutePath());
+        });
+
+        menuSave.setOnAction(event -> {
+            directoryChooser.showDialog(primaryStage);
+            File selectedDirectory = directoryChooser.showDialog(primaryStage);
+            labelSelectedDirectory.setText(selectedDirectory.getAbsolutePath());
+
+        });
+        primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
